0dc7592887de6838aa79b007b56068e4495a3897
wenshe <weiwenshe@yeteam.com>
2020-03-02 16:31:57 +0800

    fix: 修复在 deinit 中调用 weak self 导致闪退

--- ios/RNTrackPlayer/Vendor/AudioPlayer/Observer/AVPlayerItemObserver.swift
+++ ios/RNTrackPlayer/Vendor/AudioPlayer/Observer/AVPlayerItemObserver.swift
@@ -9,85 +9,81 @@ import Foundation
 import AVFoundation
 
 protocol AVPlayerItemObserverDelegate: class {
-    
-    /**
-     Called when the observed item updates the duration.
-     */
-    func item(didUpdateDuration duration: Double)
-    
+  
+  /**
+   Called when the observed item updates the duration.
+   */
+  func item(didUpdateDuration duration: Double)
+  
 }
 
 /**
  Observing an AVPlayers status changes.
  */
 class AVPlayerItemObserver: NSObject {
-    
-    private static var context = 0
-    
-    private struct AVPlayerItemKeyPath {
-        static let duration = #keyPath(AVPlayerItem.duration)
-        static let loadedTimeRanges = #keyPath(AVPlayerItem.loadedTimeRanges)
+  
+  private static var context = 0
+  
+  private struct AVPlayerItemKeyPath {
+    static let duration = #keyPath(AVPlayerItem.duration)
+    static let loadedTimeRanges = #keyPath(AVPlayerItem.loadedTimeRanges)
+  }
+  
+  var isObserving: Bool = false
+  
+  var observingItem: AVPlayerItem?
+  weak var delegate: AVPlayerItemObserverDelegate?
+  
+  deinit {
+    if self.isObserving {
+      stopObservingCurrentItem()
+    }
+  }
+  
+  /**
+   Start observing an item. Will remove self as observer from old item.
+   
+   - parameter item: The player item to observe.
+   */
+  func startObserving(item: AVPlayerItem) {
+    DispatchQueue.main.async {[weak self] in
+      guard let `self` = self else { return }
+      if self.isObserving {
+        self.stopObservingCurrentItem()
+      }
+      self.isObserving = true
+      self.observingItem = item
+      item.addObserver(self, forKeyPath: AVPlayerItemKeyPath.duration, options: [.new], context: &AVPlayerItemObserver.context)
+      item.addObserver(self, forKeyPath: AVPlayerItemKeyPath.loadedTimeRanges, options: [.new], context: &AVPlayerItemObserver.context)
+    }
+  }
+  
+  func stopObservingCurrentItem() {
+    self.observingItem?.removeObserver(self, forKeyPath: AVPlayerItemKeyPath.duration, context: &AVPlayerItemObserver.context)
+    self.observingItem?.removeObserver(self, forKeyPath: AVPlayerItemKeyPath.loadedTimeRanges, context: &AVPlayerItemObserver.context)
+    self.isObserving = false
+    self.observingItem = nil
+  } 
+  
+  override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+    guard context == &AVPlayerItemObserver.context, let observedKeyPath = keyPath else {
+      super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+      return
     }
     
-    var isObserving: Bool = false
-    
-    var observingItem: AVPlayerItem?
-    weak var delegate: AVPlayerItemObserverDelegate?
-    
-    deinit {
-        if self.isObserving {
-            stopObservingCurrentItem()
-        }
+    switch observedKeyPath {
+      case AVPlayerItemKeyPath.duration:
+        if let duration = change?[.newKey] as? CMTime {
+          self.delegate?.item(didUpdateDuration: duration.seconds)
+      }
+      
+      case AVPlayerItemKeyPath.loadedTimeRanges:
+        if let ranges = change?[.newKey] as? [NSValue], let duration = ranges.first?.timeRangeValue.duration {
+          self.delegate?.item(didUpdateDuration: duration.seconds)
+      }
+      default: break
+      
     }
-    
-    /**
-     Start observing an item. Will remove self as observer from old item.
-     
-     - parameter item: The player item to observe.
-     */
-    func startObserving(item: AVPlayerItem) {
-        DispatchQueue.main.async {[weak self] in
-            guard let `self` = self else { return }
-            if self.isObserving {
-                self.stopObservingCurrentItem()
-            }
-            self.isObserving = true
-            self.observingItem = item
-            item.addObserver(self, forKeyPath: AVPlayerItemKeyPath.duration, options: [.new], context: &AVPlayerItemObserver.context)
-            item.addObserver(self, forKeyPath: AVPlayerItemKeyPath.loadedTimeRanges, options: [.new], context: &AVPlayerItemObserver.context)
-        }
-    }
-    
-    func stopObservingCurrentItem() {
-        // 修复多线程下移除可能导致 KVO 闪退
-        DispatchQueue.main.async {[weak self] in
-            guard let `self` = self else { return }
-            self.observingItem?.removeObserver(self, forKeyPath: AVPlayerItemKeyPath.duration, context: &AVPlayerItemObserver.context)
-            self.observingItem?.removeObserver(self, forKeyPath: AVPlayerItemKeyPath.loadedTimeRanges, context: &AVPlayerItemObserver.context)
-            self.isObserving = false
-            self.observingItem = nil
-        }
-    }
-    
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        guard context == &AVPlayerItemObserver.context, let observedKeyPath = keyPath else {
-            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
-            return
-        }
-        
-        switch observedKeyPath {
-        case AVPlayerItemKeyPath.duration:
-            if let duration = change?[.newKey] as? CMTime {
-                self.delegate?.item(didUpdateDuration: duration.seconds)
-            }
-            
-        case AVPlayerItemKeyPath.loadedTimeRanges:
-            if let ranges = change?[.newKey] as? [NSValue], let duration = ranges.first?.timeRangeValue.duration {
-                self.delegate?.item(didUpdateDuration: duration.seconds)
-            }
-        default: break
-            
-        }
-    }
-    
+  }
+  
 }
