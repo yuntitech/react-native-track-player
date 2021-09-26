1952d9cb91e2b1b2c93c84b15da6ee957bc38705
leejunhui <84416097@qq.com>
2021-09-26 22:01:55 +0800

    fix: 修复 Xcode13 上编译不通过的问题

--- ios/RNTrackPlayer/Vendor/AudioPlayer/NowPlayingInfoController/NowPlayingInfoProperty.swift
+++ ios/RNTrackPlayer/Vendor/AudioPlayer/NowPlayingInfoController/NowPlayingInfoProperty.swift
@@ -31,7 +31,6 @@ public enum NowPlayingInfoProperty: NowPlayingInfoKeyValue {
      The URL pointing to the now playing item's underlying asset.
      This constant is used by the system UI when video thumbnails or audio waveform visualizations are applicable.
      */
-    @available(iOS 10.3, *)
     case assetUrl(URL?)
     
     /**
@@ -116,7 +115,6 @@ public enum NowPlayingInfoProperty: NowPlayingInfoKeyValue {
      The service provider associated with the now-playing item.
      Value is a unique NSString that identifies the service provider for the now-playing item. If the now-playing item belongs to a channel or subscription service, this key can be used to coordinate various types of now-playing content from the service provider.
      */
-    @available(iOS 11.0, *)
     case serviceIdentifier(String?)
     
     
